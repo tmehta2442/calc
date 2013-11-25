1deed628db56db284ee3cf1fda918e4e8e29f7bb
tmehta2442 <tejas@bootinguptj.com>
2013-11-25 14:46:11 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calc::Application.config.secret_token = '33c537db0ad0bed5d07a03d701a4c8dade24ef70cab4c0315c04bc3417ac479104ae2c69f71d476b314d1f4d75af2b50b8066708a6cefb49fd03f048346d79df'
