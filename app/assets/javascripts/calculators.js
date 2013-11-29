f52446309e366bad24df115dd2008db045ad0455
tmehta2442 <tmehta@post.harvard.edu>
2013-11-29 11:34:40 -0800

    updates to JS

--- app/assets/javascripts/calculators.js
+++ app/assets/javascripts/calculators.js
@@ -3,18 +3,10 @@ $(function(){
 	var $operation = $("input[name='input[operation]']");
 	var	$displayField = $("#display");
 
-
-// Talk about these three variable and how they're used in your app
 	var inputs = [];
 	var currentInput = "";
 	var action = "";
 
-/* Talk about each delegate and click event */
-// Define delegate: What does it mean. (look up jQuery Docs)
-
-	// What happens on number click before operation click
-	// What happens on number click after operation click
-	// relate this in terms of what's happening with  inputs, currentInput and action
 	$(".numbers").on("click", "button", function(event){
 		var val1 = $currentInput.val();
 
@@ -24,9 +16,6 @@ $(function(){
 		$displayField.val(currentInput);
 	});
 
-
-	// What happens on Operation Click?
-	// relate this in terms of what's happening with  inputs, currentInput and action
 	$(".operations").on("click", "button", function(event){
 		
 		var operation_value = $operation.val();
@@ -43,31 +32,29 @@ $(function(){
 		} else {
 			alert("Ooops! You've already entered an operation!")
 		}
-		console.log(currentInput)
-		console.log(inputs)
-		console.log(action)
+		// console.log(currentInput)
+		// console.log(inputs)
+		// console.log(action)
 	});
 
-
-	// What happens on Submit?
-	// relate this in terms of what's happening with  inputs, currentInput and action
 	$("#input").on("submit", function(event){
 		console.log(event)
 		event.preventDefault();
 
-		console.log(inputs)
+		// console.log(inputs)
 		inputs.push(currentInput)
-		console.log(inputs)
+		// console.log(inputs)
 
 		$.ajax({
 			url: "/"+action,
 			data: {inputs: inputs},
 			method: "get"
 		}).done(function(data) {
+			$displayField.val(data.value);
 			console.log("the result is:", data);
 		});
+		//console.log("the result is:", data);
 	});
-
 });
 
 
